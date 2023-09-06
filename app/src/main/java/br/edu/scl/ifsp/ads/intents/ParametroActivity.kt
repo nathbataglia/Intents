241052252335a17aaefc6a6eb5bc3c379e114df2
Pedro Zoia <pedrocauzivert@gmail.com>
2023-09-05 21:57:06 -0300

    Projeto com passagem de Intents entre Activities no modo legado.

---
+++ app/src/main/java/br/edu/scl/ifsp/ads/intents/ParametroActivity.kt
@@ -0,0 +1,30 @@
+package br.edu.scl.ifsp.ads.intents
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.scl.ifsp.ads.intents.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.ads.intents.databinding.ActivityParametroBinding
+
+class ParametroActivity : AppCompatActivity() {
+    private val apb by lazy {
+        ActivityParametroBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apb.root)
+        supportActionBar?.subtitle = "ParametroActivity"
+
+        intent.getStringExtra(MainActivity.PARAMETRO_EXTRA)?.let {parametro ->
+            apb.parametroEt.setText(parametro)
+        }
+
+        apb.enviarParametroBt.setOnClickListener {
+            val intentRetorno = Intent()
+            intentRetorno.putExtra(MainActivity.PARAMETRO_EXTRA, apb.parametroEt.text.toString())
+            setResult(RESULT_OK, intentRetorno)
+            finish()
+        }
+    }
+}
